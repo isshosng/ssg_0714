a2fd64377636b46c2d4afebad31e013805316043
doxxx <doxxx93@gmail.com>
2022-07-15 13:40:55 +0900

    TestUtil을 통해 입출력 리다이렉팅 테스트

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,39 @@
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AppTest {
+    @Test
+    @DisplayName("스캐너에 키보드 입력이 아닌 문자열 입력으로 설정")
+    public void scannerWithString() {
+        // given
+        Scanner sc = TestUtil.genScanner("안녕");
+
+        // when
+        String cmd = sc.nextLine().trim();
+
+        // then
+        assertThat(cmd).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("출력을 콘솔이 아닌 문자열로 얻기")
+    void outputStreamWithString() {
+        //given
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        //when
+        System.out.print("안녕");
+        String s = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        //then
+        assertThat(s).isEqualTo("안녕");
+    }
 
 }
