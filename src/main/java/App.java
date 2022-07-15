2019871e10d67e060bb9bb01a2b33d79f52ac38e
doxxx <doxxx93@gmail.com>
2022-07-15 14:10:50 +0900

    등록시 명언과 작가 입력 요구 테스트

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,7 +19,16 @@ public class App {
             switch (cmd) {
                 case "종료":
                     break outer;
+                case "등록":
+                    write();
             }
         }
+    }
+
+    private void write() {
+        System.out.print("명언을 입력하세요: ");
+        String content = sc.nextLine().trim();
+        System.out.print("작가를 입력하세요: ");
+        String author = sc.nextLine().trim();
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
+
     @Test
     @DisplayName("스캐너에 키보드 입력이 아닌 문자열 입력으로 설정")
     public void scannerWithString() {
@@ -35,12 +36,13 @@ class AppTest {
         //then
         assertThat(s).isEqualTo("안녕");
     }
+
     @Test
     @DisplayName("문자열을 파일에 저장")
     void saveStringToFile() {
         //given
         Util.file.mkdir("test_data");
-        Util.file.saveToFile("test_data/1.txt","안녕");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
         //when
         String body = Util.file.readFromFile("test_data/1.txt");
 
@@ -62,6 +64,27 @@ class AppTest {
         TestUtil.clearSetOutToByteArray(output);
         //then
         assertThat(s).isEqualTo("== 명언 SSG ==\n명령) ");
+    }
+
+    @Test
+    @DisplayName("등록시 명언과 작가 입력을 요구한다.")
+    void writeTest() {
+        //given
+        Scanner sc = TestUtil.genScanner("""
+            등록
+            배고파
+            김도율
+            종료
+            """);
+        ByteArrayOutputStream out = TestUtil.setOutToByteArray();
+        //when
+        new App(sc).run();
+        String s = out.toString();
+        TestUtil.clearSetOutToByteArray(out);
+
+        //then
+        assertThat(s).contains("명언을 입력하세요: ");
+        assertThat(s).contains("작가를 입력하세요: ");
     }
 
 }
